887a90758a8c02984353f7e0f50e322337c54dc6
dhjr <dhananjayrjs1605@gmail.com>
2025-12-16 18:49:58 +0530

    minor fixes

--- script.js
+++ script.js
@@ -321,6 +321,6 @@ document.getElementById('calcBtn').addEventListener('click', function() {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/serviceWorker.js");
+    navigator.serviceWorker.register("./serviceWorker.js");
   });
 }
